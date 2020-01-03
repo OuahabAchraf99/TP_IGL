e3ad6f10e6ecf018eeaf689e0c85880e87443708
OuahabAchraf99 <ouahabachraf99@gmail.com>
2020-01-03 01:49:59 +0100

    midlleware was modified

--- BackEnd/app/Http/Middleware/VerifyCsrfToken.php
+++ BackEnd/app/Http/Middleware/VerifyCsrfToken.php
@@ -18,7 +18,7 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-    protected $except = [ '/Etudiant'
+    protected $except = [ '/Etudiant','/uploadfile'
         //
     ];
 }
